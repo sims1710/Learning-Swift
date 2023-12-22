5e7ee60ba513928acb276f7c0850cd3f5cd4ae5a
Simriti <simritib1@gmail.com>
2023-12-22 15:48:20 +0400

    Create compoundAssignmentOperators.swift

---
+++ Variables/compoundAssignmentOperators.swift
@@ -0,0 +1,21 @@
+var dollars = 5
+dollars += 4   // same as dollars = 5 + 4
+print(dollars) // Prints: 9
+
+dollars -= 3   // same as dollars = 9 - 3
+print(dollars) // Prints: 6
+
+dollars *= 5   // same as dollars = 6 * 5
+print(dollars) // Prints: 30
+
+dollars /= 6   // same as dollars = 30 / 6
+print(dollars) // Prints: 5
+
+dollars %= 2   // same as dollars = 5 % 2
+print(dollars) // Prints: 1
+
+var apples = 16
+apples -= 4
+apples /= 2
+
+print(apples)
