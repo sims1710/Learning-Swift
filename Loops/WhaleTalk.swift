03d01d1589e3367d9b031f51fafef225e71b611c
Simriti <simritib1@gmail.com>
2023-12-25 09:38:29 +0400

    Create WhaleTalk.swift

---
+++ Loops/WhaleTalk.swift
@@ -0,0 +1,17 @@
+var input = "Hello"
+var output = ""
+
+for char in input {
+  let lowerChar = char.lowercased()
+  switch lowerChar {
+    case "a", "i", "o":
+      output += lowerChar.uppercased()
+    case "e":
+      output += "EE"
+    case "u":
+      output += "UU"
+    default:
+      continue
+  }
+}
+print(output)
