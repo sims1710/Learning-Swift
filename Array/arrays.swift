b5cfbf795e1b326071d4178161bf9b275d0ee132
Simriti <simritib1@gmail.com>
2023-12-25 09:57:57 +0400

    Update arrays.swift

--- Array/arrays.swift
+++ Array/arrays.swift
@@ -1,1 +1,33 @@
+// Creating an Empty Array
+var name = [Type]()
 
+var scores = [Int]()
+var subwayAdult = [Int]()
+print(subwayAdult)
+
+var subwayChild = [Int]()
+print(subwayChild)
+
+// Creating an Array Literal
+var name = [list of values]
+
+var location: [Double] = [40.7245, -73.9979]
+
+var subwayAdult = [800, 1200, 1500] 
+print(subwayAdult)
+
+var subwayChild: [Int] = [400, 600, 750]
+print(subwayChild)
+
+// Index
+var vowels = ["a", "e", "i", "o", "u"]
+
+/*
+The item at index 0 is "a".
+The item at index 1 is "e".
+The item at index 2 is "i".
+The item at index 3 is "o".
+The item at index 4 is "u".
+*/
+
+// .count Property : to give us a number value of elements inside the array.
