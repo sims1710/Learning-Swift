4730fa4b94f1ca47dafbd4fb95bf017c4357672e
Simriti <simritib1@gmail.com>
2023-12-25 09:43:58 +0400

    Create Bottles.swift

---
+++ Loops/Bottles.swift
@@ -0,0 +1,10 @@
+var numBottles: Int = 99
+while numBottles > 0 {
+  print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
+  print("You take one down and pass it around…")
+  print("\(numBottles) bottles of milk on the wall!")
+  numBottles -= 1
+}
+
+print("0 bottles of milk on the wall, 0 bottles of milk!")
+print("Go to the store and buy some more, 99 bottles of milk on the wall!")
