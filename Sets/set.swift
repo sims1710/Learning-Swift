15e34fbbf8f7d6071b1dde6d80c66a72d2bc1b47
Simriti <simritib1@gmail.com>
2024-03-09 21:52:15 +0800

    Update set.swift

--- Sets/set.swift
+++ Sets/set.swift
@@ -1,1 +1,9 @@
+// The syntax for creating an empty set looks like this:
+var setName = Set<Type>()
 
+var instruments = Set<String>()
+var setName: Set = [Value1, Value2, Value3]
+
+var consonants = Set<Character>()
+var vowels: Set = ["A", "E", "I", "O", "U"]
+print(vowels)
